864649526ec2e52f92fc5172344b39b6f61064d9
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-12-10 18:36:00 -0600

    Testing completed

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -1,8 +1,8 @@
 from django.db import models
 from .artist import Artist
-
 class Song(models.Model):
-  title = models.CharField(max_length=200)
-  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-  album = models.CharField(max_length=50)
-  length = models.PositiveIntegerField()
+
+    title = models.CharField(max_length=50)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    album = models.CharField(max_length=50)
+    length = models.IntegerField()
--- tunaapi/views/song_genre.py
+++ tunaapi/views/song_genre.py
@@ -1,37 +1,38 @@
-"""View module for handling requests about songgenre"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import SongGenre, Song, Genre
+from tunaapi.models import SongGenre
 
 
 class SongGenreView(ViewSet):
-    """Tuna Genre view"""
+    """Tuna api song genre view"""
 
     def retrieve(self, request, pk):
-        """Handle GET requests for songgenre
-
+        """Handle GET requests for single song
         Returns:
-            Response -- JSON serialized songgenre
+            Response -- JSON serialized song
         """
-        songgenre = SongGenre.objects.get(pk=pk)
-        serializer = SongGenreSerializer(songgenre)
-        return Response(serializer.data)
+        try:
+            song_genre = SongGenre.objects.get(pk=pk)
+            serializer = SongGenreSerializer(song_genre)
+            return Response(serializer.data)
+        except SongGenre.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
     def list(self, request):
-        """Handle GET requests to get all songgenres
-
+        """Handle GET requests to get all songs
         Returns:
-            Response -- JSON serialized list of songgenres
+            Response -- JSON serialized list of songs
         """
-        songgenre = SongGenre.objects.all()
-        serializer = SongGenreSerializer(songgenre, many=True)
+        song_genres = SongGenre.objects.all()
+        serializer = SongGenreSerializer(song_genres, many=True)
         return Response(serializer.data)
-       
+        
+
 class SongGenreSerializer(serializers.ModelSerializer):
-    """JSON serializer for song
+    """JSON serializer for song genres
     """
     class Meta:
         model = SongGenre
-        fields = ('id', 'song_id', 'genre_id')
+        fields = ('song', 'genre')
